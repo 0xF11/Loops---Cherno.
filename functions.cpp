83cce77a8a50aeb6afc0fdd67815cb41aa962b38
0xF11 <140972657+0xF11@users.noreply.github.com>
2025-05-30 15:07:22 -0700

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <windows.h>
+
+
+void textel(std::string message)
+{
+	std::cout << message << std::endl;
+}
+void text(std::string messager)
+{
+	std::cout << messager;
+}
